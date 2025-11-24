67e14362796c4011d7c66182598cbc1ae52605d8
cdstk <cdstkemail@gmail.com>
2025-11-23 21:12:20 -0600

    Bump 1.0.1

--- src/main/java/balancedunbreakable/BalancedUnbreakable.java
+++ src/main/java/balancedunbreakable/BalancedUnbreakable.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = BalancedUnbreakable.MODID, version = BalancedUnbreakable.VERSION, name = BalancedUnbreakable.NAME, dependencies = "required-after:fermiumbooter")
 public class BalancedUnbreakable {
     public static final String MODID = "balancedunbreakable";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String NAME = "BalancedUnbreakable";
     public static final Logger LOGGER = LogManager.getLogger();
 
